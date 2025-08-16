741c90da58c7e2cf9ac2d280e985115f25dd38ff
Charlesovaga <ovagacharles@gmail.com>
2025-08-16 12:41:50 +0100

    Kafka backend done

--- apps/kafka-service/src/main.ts
+++ apps/kafka-service/src/main.ts
@@ -1,21 +1,57 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import {kafka} from "@packages/utils/kafka"
+import { updateUserAnalytics } from "./services/analytics.service";
 
-import express from 'express';
-import * as path from 'path';
 
-const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const consumer = kafka.consumer({groupId: "user-events-group"})
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to kafka-service!' });
-});
+const eventQueue: any[] = [];
 
-const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+const processQueue = async() => {
+  if(eventQueue.length === 0) return;
+
+  const events = [...eventQueue]
+  eventQueue.length = 0
+
+  for (const event of events){
+    if(event.action === "shop_visit"){
+      // Update shop anyltics
+    }
+
+    const validActions = [
+      "add_to_wishlist",
+      "add_to_cart",
+      "product_view",
+      "remove_from_wishlist",
+    ];
+
+    if(!event.action || !validActions.includes(event.action)){
+      continue
+    }
+    try {
+      await updateUserAnalytics(event)
+    } catch (error) {
+      console.log("error processing event:", error)
+    }
+  }
+};
+
+setInterval(processQueue, 3000);  //  3000 ms = 3s
+
+// Kafka consumer events
+export const consumerKafkaMessages = async () => {
+  // connect to the kafka broker
+
+  await consumer.connect()
+  await consumer.subscribe({topic: "users-events", fromBeginning: false})
+
+  await consumer.run({
+    eachMessage: async({message}) => {
+      if(!message.value) return
+      const event = JSON.parse(message.value.toString());
+      eventQueue.push(event)
+    }
+  })
+}
+
+consumerKafkaMessages().catch(console.error)
